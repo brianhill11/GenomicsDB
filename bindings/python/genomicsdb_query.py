861020ecdeb6a689dd10c92dd61e94a5f4770b23
Brian Hill <brian.l.hill11@gmail.com>
2018-07-03 17:21:29 -0700

    Added return types, changed way config is passed to functions, added search for tiledbgenomicsdb library

--- bindings/python/genomicsdb_query.py
+++ bindings/python/genomicsdb_query.py
@@ -1,5 +1,8 @@
 from ctypes import *
-gdb_dll = CDLL("libtiledbgenomicsdb.so")
+from ctypes.util import find_library
+
+tiledb_genomicsdb_lib_path = find_library("tiledbgenomicsdb")
+gdb_dll = CDLL(tiledb_genomicsdb_lib_path)
 
 
 def connect(
@@ -18,11 +21,15 @@ def connect(
         # set return type for library function calls
         gdb_dll.genomicsdb_create_config.restype = c_void_p
         gdb_dll.genomicsdb_connect.restype = c_void_p
-        # get pointer to config instance 
-        config = gdb_dll.genomicsdb_create_config(byref(create_string_buffer(loader_config_file)),
-                                                  byref(create_string_buffer(query_config_file)),
-                                                  byref(create_string_buffer(chr)),
-                                                  byref(c_int(start)), byref(c_int(end)))
+        # get pointer to config instance
+        print "qcf:", query_config_file
+        loader_config_file_buff = create_string_buffer(loader_config_file)
+        query_config_file_buff = create_string_buffer(query_config_file)
+        chr_buff = create_string_buffer(str(chr))
+        config = gdb_dll.genomicsdb_create_config(byref(loader_config_file_buff),
+                                                  byref(query_config_file_buff),
+                                                  byref(chr_buff),
+                                                  c_int(start), c_int(end))
         if (rank is not None):
                 gdb_dll.genomicsdb_config_set_rank(byref(c_void_p(config)), c_int(rank))
         if (buffer_capacity is not None):
@@ -32,7 +39,7 @@ def connect(
         if (is_bcf is not None):
                 gdb_dll.genomicsdb_config_set_is_bcf(byref(c_void_p(config)), c_int(is_bcf))
         if (produce_header_only is not None):
-                gdb_dll.genomicsdb_config_set_produce_header_only(config, c_int(produce_header_only))
+                gdb_dll.genomicsdb_config_set_produce_header_only(byref(c_void_p(config)), c_int(produce_header_only))
         if (use_missing_values_only_not_vector_end is not None):
                 gdb_dll.genomicsdb_config_set_use_missing_values_only_not_vector_end(byref(c_void_p(config)),
                                                                     c_int(use_missing_values_only_not_vector_end))
@@ -41,6 +48,7 @@ def connect(
 
 
 def close(handle):
+    gdb_dll.genomicsdb_close.restype = c_int
     gdb_dll.genomicsdb_close(byref(c_void_p(handle)))
 
 
@@ -50,12 +58,16 @@ def total_bytes_available(handle):
 
 
 def read_next_byte(handle):
+    gdb_dll.genomicsdb_read_next_byte.restype = c_uint8
     return gdb_dll.genomicsdb_read_next_byte(byref(c_void_p(handle)))
 
 
 def read(handle, byte_array, offset, n):
-    return gdb_dll.genomicsdb_read(byref(c_void_p(handle)), c_char_p(byte_array), c_ulong_long(offset), c_int(n))
+    gdb_dll.genomicsdb_read.restype = c_int
+    # byte array should be a mutable string buffer (i.e. from ctypes.create_string_buffer)
+    return gdb_dll.genomicsdb_read(byref(c_void_p(handle)), byte_array, c_uint64(offset), c_uint64(n))
 
 
 def skip(handle, n):
+    gdb_dll.genomicsdb_skip.restype = c_uint64
     return gdb_dll.genomicsdb_skip(byref(c_void_p(handle)), c_int(n))
